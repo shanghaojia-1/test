4602923b89790eefd4ba90ab121a8116c1f226aa
shanghaojia-1 <1448031411@qq.com>
2021-08-26 14:15:57 +0800

    Create onekey1.sh

---
+++ onekey1.sh
@@ -0,0 +1,11 @@
+apt update
+apt install docker.io -y
+
+for ((i=$1; i<=$2; i++))
+do
+   docker run -dit \
+  --name peer$i \
+  --hostname  peer$i \
+  --restart always \
+  shanghaojia1/mypeer1
+done
